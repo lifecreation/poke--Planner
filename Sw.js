706b74df1027784d2b47a248193b97f205056623
lifecreation <lifecreation26@gmail.com>
2026-09-07 00:49:45 +0530

    Add files via upload

---
+++ Sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'raat-ka-plan-v1';
+const ASSETS = ['./index.html', './Mainfast.json'];
+    
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
