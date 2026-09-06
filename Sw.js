db53bb9d1cea1b25422eba24ca6e5721f4e9fde7
lifecreation <lifecreation26@gmail.com>
2026-09-07 01:02:52 +0530

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,6 +1,6 @@
 const CACHE = 'raat-ka-plan-v1';
-const ASSETS = ['./index.html', './Mainfast.json'];
-    
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
