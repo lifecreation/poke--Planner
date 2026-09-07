e812b07763a618f226a5cc5f08f5df5d5e1f97b2
lifecreation <lifecreation26@gmail.com>
2026-09-07 07:52:21 +0530

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'raat-ka-plan-v1';
-const ASSETS = ['./index.html', './Mainfast.json', './icon-192.png', './icon-512.png'];
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
@@ -13,5 +13,17 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((list) => {
+      for (const client of list) {
+        if ('focus' in client) return client.focus();
+      }
+      if (clients.openWindow) return clients.openWindow('./index.html');
+    })
   );
 });
